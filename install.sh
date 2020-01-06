c289721e0f0e4869f34eb80407edb2974aa91bf0
Evan Sloan <evansloan082@gmail.com>
2020-01-06 10:37:01 -0500

    add version arg

--- install.sh
+++ install.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
-go build
+version="v0.0.1"
+commit=`git rev-parse --short HEAD`
+
+go build -ldflags="-X main.version=${version}  -X main.commit=${commit}"
 sudo cp nfl-term $HOME/bin
 rm nfl-term
--- main.go
+++ main.go
@@ -1,6 +1,27 @@
 package main
 
+import (
+	"fmt"
+	"os"
+	"strings"
+)
+
+var (
+	version string
+	commit  string
+)
+
 func main() {
+
+	if len(os.Args) > 1 {
+		arg := os.Args[1]
+		if strings.ToLower(arg) == "version" {
+			fmt.Println("Version: ", version)
+			fmt.Println("Commit: ", commit)
+			os.Exit(3)
+		}
+	}
+
 	app := NewApp()
 	app.RunApp()
 }
