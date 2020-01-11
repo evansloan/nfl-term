9843f596ad57a201cbb9ed4e61e8e40c16751f7b
Evan Sloan <evansloan082@gmail.com>
2020-01-10 22:08:29 -0500

    add special teams page

--- ui/playerstats.go
+++ ui/playerstats.go
@@ -6,6 +6,19 @@ import (
 	"github.com/evansloan/nfl-term/api"
 )
 
+type StatType int
+
+const (
+	Passing   StatType = 0
+	Rushing   StatType = 1
+	Receiving StatType = 2
+	Defense   StatType = 3
+	Kicking   StatType = 4
+	Punting   StatType = 5
+	KickRet   StatType = 6
+	PuntRet   StatType = 7
+)
+
 // PlayerStats represents a UI element that displays
 // the player stats from a specific NFL game.
 // Can display passing/rushing/receiving stats.
@@ -13,31 +26,33 @@ import (
 // Inherits from GenericTable
 type PlayerStats struct {
 	*DefaultTable
+	StatType StatType
 	SetStats func(game *api.Game)
 }
 
 // NewPlayerStats creates a new PlayerStats UI element
-//
-// Valid statType values:
-//		0 = passing
-//		1 = rushing
-//		2 = receiving
-//
-func NewPlayerStats(title string, statType int) *PlayerStats {
+func NewPlayerStats(title string, statType StatType) *PlayerStats {
 	p := &PlayerStats{
 		DefaultTable: NewGenericTable(title),
+		StatType:     statType,
 	}
 
 	p.SetFixed(1, 0)
 
-	if statType == 0 {
+	if statType == Passing {
 		p.setHandler(p.setPassStats)
-	} else if statType == 1 {
+	} else if statType == Rushing {
 		p.setHandler(p.setRushStats)
-	} else if statType == 2 {
+	} else if statType == Receiving {
 		p.setHandler(p.setRecvStats)
-	} else if statType == 3 {
+	} else if statType == Defense {
 		p.setHandler(p.setDefStats)
+	} else if statType == Kicking {
+		p.setHandler(p.setKickStats)
+	} else if statType == Punting {
+		p.setHandler(p.setPuntStats)
+	} else if statType == KickRet || statType == PuntRet {
+		p.setHandler(p.setReturnStats)
 	}
 
 	return p
@@ -60,7 +75,7 @@ func (p *PlayerStats) setPassStats(game *api.Game) {
 		SetHeaderCell(0, 4, "TDs").
 		SetHeaderCell(0, 5, "Ints")
 
-	stats := sortPlayers(game.Home.Stats.Passing, game.Away.Stats.Passing, 0)
+	stats := p.sortPlayers(game.Home.Stats.Passing, game.Away.Stats.Passing)
 
 	for i, player := range stats {
 		p.SetTextCell(i+1, 0, player.Name).
@@ -82,7 +97,7 @@ func (p *PlayerStats) setRushStats(game *api.Game) {
 		SetHeaderCell(0, 2, "Yards").
 		SetHeaderCell(0, 3, "TDs")
 
-	stats := sortPlayers(game.Home.Stats.Rushing, game.Away.Stats.Rushing, 0)
+	stats := p.sortPlayers(game.Home.Stats.Rushing, game.Away.Stats.Rushing)
 
 	for i, player := range stats {
 		p.SetTextCell(i+1, 0, player.Name).
@@ -102,7 +117,7 @@ func (p *PlayerStats) setRecvStats(game *api.Game) {
 		SetHeaderCell(0, 2, "Yards").
 		SetHeaderCell(0, 3, "TDs")
 
-	stats := sortPlayers(game.Home.Stats.Receiving, game.Away.Stats.Receiving, 0)
+	stats := p.sortPlayers(game.Home.Stats.Receiving, game.Away.Stats.Receiving)
 
 	for i, player := range stats {
 		p.SetTextCell(i+1, 0, player.Name).
@@ -124,7 +139,7 @@ func (p *PlayerStats) setDefStats(game *api.Game) {
 		SetHeaderCell(0, 4, "Ints").
 		SetHeaderCell(0, 5, "FFum")
 
-	stats := sortPlayers(game.Home.Stats.Defense, game.Away.Stats.Defense, 1)
+	stats := p.sortPlayers(game.Home.Stats.Defense, game.Away.Stats.Defense)
 
 	for i, player := range stats {
 		p.SetTextCell(i+1, 0, player.Name).
@@ -137,9 +152,78 @@ func (p *PlayerStats) setDefStats(game *api.Game) {
 	p.ScrollToBeginning()
 }
 
+func (p *PlayerStats) setKickStats(game *api.Game) {
+	p.Clear()
+
+	p.SetHeaderCell(0, 0, "Name").
+		SetHeaderCell(0, 1, "FG Made").
+		SetHeaderCell(0, 2, "FG Att").
+		SetHeaderCell(0, 3, "XP Made").
+		SetHeaderCell(0, 4, "XP Att")
+
+	stats := p.sortPlayers(game.Home.Stats.Kicking, game.Away.Stats.Kicking)
+
+	for i, player := range stats {
+		p.SetTextCell(i+1, 0, player.Name).
+			SetIntCell(i+1, 1, player.FGMade).
+			SetIntCell(i+1, 2, player.FGAtt).
+			SetIntCell(i+1, 3, player.XPMade).
+			SetIntCell(i+1, 4, player.XPAtt)
+	}
+	p.ScrollToBeginning()
+}
+
+func (p *PlayerStats) setPuntStats(game *api.Game) {
+	p.Clear()
+
+	p.SetHeaderCell(0, 0, "Name").
+		SetHeaderCell(0, 1, "Punts").
+		SetHeaderCell(0, 2, "Yards").
+		SetHeaderCell(0, 3, "Inside 20")
+
+	stats := p.sortPlayers(game.Home.Stats.Punting, game.Away.Stats.Punting)
+
+	for i, player := range stats {
+		p.SetTextCell(i+1, 0, player.Name).
+			SetIntCell(i+1, 1, player.Punts).
+			SetIntCell(i+1, 2, player.Yards).
+			SetIntCell(i+1, 3, player.Inside20)
+	}
+	p.ScrollToBeginning()
+}
+
+func (p *PlayerStats) setReturnStats(game *api.Game) {
+	p.Clear()
+
+	p.SetHeaderCell(0, 0, "Name").
+		SetHeaderCell(0, 1, "Returns").
+		SetHeaderCell(0, 2, "Avg").
+		SetHeaderCell(0, 3, "Yards").
+		SetHeaderCell(0, 4, "TDs").
+		SetHeaderCell(0, 5, "Long")
+
+	var stats []api.Player
+
+	if p.StatType == KickRet {
+		stats = p.sortPlayers(game.Home.Stats.KickRet, game.Away.Stats.KickRet)
+	} else if p.StatType == PuntRet {
+		stats = p.sortPlayers(game.Home.Stats.PuntRet, game.Away.Stats.PuntRet)
+	}
+
+	for i, player := range stats {
+		p.SetTextCell(i+1, 0, player.Name).
+			SetIntCell(i+1, 1, player.Returns).
+			SetIntCell(i+1, 2, player.Avg).
+			SetIntCell(i+1, 3, player.Yards).
+			SetIntCell(i+1, 4, player.Tds).
+			SetIntCell(i+1, 5, player.Long)
+	}
+	p.ScrollToBeginning()
+}
+
 // sortPlayers sorts players by passing/rushing/receiving yards
 // in descending order.
-func sortPlayers(home, away map[string]api.Player, statType int) []api.Player {
+func (p *PlayerStats) sortPlayers(home, away map[string]api.Player) []api.Player {
 	var stats []api.Player
 
 	for _, player := range home {
@@ -150,10 +234,16 @@ func sortPlayers(home, away map[string]api.Player, statType int) []api.Player {
 	}
 
 	sort.Slice(stats[:], func(i, j int) bool {
-		if statType == 0 {
+		if p.StatType == Passing || p.StatType == Rushing || p.StatType == Receiving {
 			return stats[i].Yards > stats[j].Yards
-		} else if statType == 1 {
+		} else if p.StatType == Defense {
 			return stats[i].Tkl > stats[j].Tkl
+		} else if p.StatType == Kicking {
+			return stats[i].FGMade > stats[j].FGMade
+		} else if p.StatType == Punting {
+			return stats[i].Punts > stats[j].Punts
+		} else if p.StatType == KickRet || p.StatType == PuntRet {
+			return stats[i].Att > stats[j].Att
 		}
 		return []rune(stats[i].Name)[0] < []rune(stats[j].Name)[0]
 	})
--- ui/statpages.go
+++ ui/statpages.go
@@ -11,18 +11,18 @@ type statPage struct {
 	selected int
 }
 
-func newStatPage(stats []*PlayerStats, colSpan int) *statPage {
+func newStatPage(stats []*PlayerStats, cols ...int) *statPage {
 	s := &statPage{
 		Grid:     tview.NewGrid(),
 		Stats:    stats,
 		selected: -1,
 	}
 
-	s.SetColumns(40, 30, 30, 0)
+	s.SetColumns(cols...)
 
 	for i, item := range stats {
 		item.SetSelectable(true, false)
-		s.AddItem(item, 0, i, 3, colSpan, 0, 0, true)
+		s.AddItem(item, 0, i, 3, 1, 0, 0, true)
 	}
 
 	return s
@@ -41,6 +41,7 @@ type StatPages struct {
 	*tview.Pages
 	oPage      *statPage
 	dPage      *statPage
+	stPage     *statPage
 	ActivePage *statPage
 }
 
@@ -50,18 +51,26 @@ func NewStatPages() *StatPages {
 	}
 
 	oStats := []*PlayerStats{
-		NewPlayerStats("Passing", 0),
-		NewPlayerStats("Rushing", 1),
-		NewPlayerStats("Receiving", 2),
+		NewPlayerStats("Passing", Passing),
+		NewPlayerStats("Rushing", Rushing),
+		NewPlayerStats("Receiving", Receiving),
 	}
 	dStats := []*PlayerStats{
-		NewPlayerStats("Defense", 3),
+		NewPlayerStats("Defense", Defense),
 	}
-	s.oPage = newStatPage(oStats, 1)
-	s.dPage = newStatPage(dStats, 2)
+	stStats := []*PlayerStats{
+		NewPlayerStats("Kicking", Kicking),
+		NewPlayerStats("Punting", Punting),
+		NewPlayerStats("Kick return", KickRet),
+		NewPlayerStats("Punt return", PuntRet),
+	}
+	s.oPage = newStatPage(oStats, 40, 30, 30, 0)
+	s.dPage = newStatPage(dStats, 60, 0)
+	s.stPage = newStatPage(stStats, 30, 30, 30, 30, 0)
 
 	s.AddPage("opage", s.oPage, true, true)
 	s.AddPage("dpage", s.dPage, true, false)
+	s.AddPage("stpage", s.stPage, true, false)
 	s.ActivePage = s.oPage
 
 	return s
@@ -70,8 +79,10 @@ func NewStatPages() *StatPages {
 func (s *StatPages) SetActive(pageName string) {
 	if pageName == "opage" {
 		s.ActivePage = s.oPage
-	} else {
+	} else if pageName == "dpage" {
 		s.ActivePage = s.dPage
+	} else if pageName == "stpage" {
+		s.ActivePage = s.stPage
 	}
 }
 
@@ -80,6 +91,9 @@ func (s *StatPages) SetStats(game *api.Game) {
 		stat.SetStats(game)
 	}
 	for _, stat := range s.dPage.Stats {
+		stat.SetStats(game)
+	}
+	for _, stat := range s.stPage.Stats {
 		stat.SetStats(game)
 	}
 }
